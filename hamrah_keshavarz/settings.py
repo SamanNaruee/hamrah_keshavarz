5dd5ec5cce346d5d6fb9ae1999652f9e7b2fb7b7
saman <samannaruee@gmail.com>
2025-03-13 15:46:40 +0330

    Add swagger and postgres settings: did not test yet

--- hamrah_keshavarz/settings.py
+++ hamrah_keshavarz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+    'drf_yasg',
+
+    # Local apps
 ]
 
 MIDDLEWARE = [
@@ -73,12 +80,16 @@ WSGI_APPLICATION = 'hamrah_keshavarz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASS'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }  
+}  
 
 
 # Password validation
