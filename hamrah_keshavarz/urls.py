04137063ae99e7ef687fd274484cf7f5b85ae85b
saman <samannaruee@gmail.com>
2025-03-13 16:03:13 +0330

    Update : add jwt settings and add requi.txt file.

--- hamrah_keshavarz/urls.py
+++ hamrah_keshavarz/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from rest_framework import permissions  
 from drf_yasg.views import get_schema_view  
 from drf_yasg import openapi  
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(  
     openapi.Info(  
@@ -37,5 +38,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  
 ]
