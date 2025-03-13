8203d79921f3efc5cc5d2f36d5a8de7e65c28adc
saman <samannaruee@gmail.com>
2025-03-13 16:55:58 +0330

    Add farmer model.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
+import uuid
+from datetime import datetime, timedelta
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class Farmer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(
+        max_length=20,
+        unique=True,
+        blank=True,
+        null=True,
+        validators=[
+            models.RegexValidator(
+                regex=r'^0\d{11}$|^\+98\d{13}$',
+                message='phone number must be 11 digits and starts with 0 or 13 digits and starts with +98',
+                code='invalid_phone_number'
+            )
+        ],
+    )
+
+    def __str__(self):
+        return self.name
